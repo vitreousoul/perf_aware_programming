63b9f5d3c8686c38aeb1816c948656bdd081b60d
Ryan Merritt <merrittaudio@gmail.com>
2023-12-02 11:39:01 -0600

    simple loop bandwidth test

--- src/asm_loop_test.asm
+++ src/asm_loop_test.asm
@@ -5,7 +5,6 @@ section .text
 TestLoop:
     xor rax, rax
 loop:
-    mov [rsi + rax], al
     add rax, 1
     cmp rdi, rax
     jne loop
--- src/asm_loop_test.c
+++ src/asm_loop_test.c
@@ -1,28 +1,46 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "ryn_prof.h"
+
+typedef enum
+{
+    TB_main,
+} timed_block_kind;
+
 typedef struct
 {
     size_t Size;
     char *Data;
 } buffer;
 
-char Data[256] = {};
+#define BUFFER_SIZE 4096
+
+char Data[BUFFER_SIZE] = {};
 
 void TestLoop(buffer Buffer);
 
 int main(void)
 {
     buffer Buffer;
-    Buffer.Size = 32;
+    Buffer.Size = BUFFER_SIZE;
     Buffer.Data = Data;
 
-    TestLoop(Buffer);
+    BeginProfile();
 
+    BEGIN_BANDWIDTH_BLOCK(TB_main, BUFFER_SIZE);
+    TestLoop(Buffer);
+    END_TIMED_BLOCK(TB_main);
+
+    EndAndPrintProfile();
+
+    /* printf("%zu\n", Buffer.Size); */
+
+#if 0
     for (int I = 0; I < Buffer.Size; ++I)
     {
         printf("%d ", Buffer.Data[I]);
     }
-
     printf("\n");
+#endif
 }
